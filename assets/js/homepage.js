8875f94ba24a7f47640e8c26d2d085e629c3e38b
Michael Dreesen <michaeldreesen90@gmail.com>
2020-08-24 13:36:23 -0600

    new function request and adds a parameter

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,15 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-};
+var getUserRepos = function(user) {
+    // formatting the API url differently from
+    // fetch("https://api.github.com/users/octocat/repos").then(function(response)
+    // This adds a parameter
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-getUserRepos();
+    // Make a request to the URL
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        })
+    })
+}
+
+getUserRepos("github");
